ad5a6a3e158fcd16df6ff3b1528e04ed9bc279b2
Mattias Berglund <berglund92@outlook.com>
2019-07-25 11:48:24 +0200

    Battlelog and battleevent added

---
+++ MySamurai2.Domain/BattleEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MySamurai2.Domain
+{
+    public class BattleEvent
+    {
+        public int Id { get; set; }
+        [StringLength(1000)]
+        public string Summary { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ MySamurai2.Domain/BattleLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySamurai2.Domain
+{
+    public class BattleLog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int BattleId { get; set; }
+        public Battle Battle { get; set; }
+
+        public List<BattleEvent> BattleEvents { get; set; }
+    }
+}
