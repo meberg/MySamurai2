bd6fb7513b5d6d5b395a9253c152c34ec04153aa
Mattias Berglund <berglund92@outlook.com>
2019-07-25 11:16:12 +0200

    AddedBattle

---
+++ MySamurai2.Domain/Battle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MySamurai2.Domain
+{
+    class Battle
+    {
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Brutal { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
